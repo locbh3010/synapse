d6927b168458220a42064d641cfa24cddb2a61c8
Loc Huu <locbh3010@gmail.com>
2024-02-04 20:59:34 +0700

    add typography

---
+++ src/components/primitives/Typography/Text.tsx
@@ -0,0 +1,37 @@
+import { cva } from 'class-variance-authority'
+import { ReactNode, forwardRef } from 'react'
+import cn from 'classnames'
+
+const textClasses = cva('font-normal', {
+	variants: {
+		size: {
+			xxs: 'text-[10px] leading-[1.2]',
+			xs: 'text-xs leading-[1.16]',
+			sm: 'text-sm leading-[1.14]',
+			md: 'text-base leading-[1.14]',
+			lg: 'text-lg leading-[1.11]',
+			xl: 'text-xl leading-[1.2]'
+		}
+	}
+})
+
+interface ITextProps {
+	size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+	children: ReactNode
+	className?: string
+	[key: string]: any
+}
+
+const Text = forwardRef<HTMLParagraphElement, ITextProps>(
+	({ size = 'md', children, className, ...rest }, ref) => {
+		return (
+			<p {...rest} ref={ref} className={cn(textClasses({ size, className }))}>
+				{children}
+			</p>
+		)
+	}
+)
+
+Text.displayName = 'Text'
+
+export default Text
---
+++ src/components/primitives/Typography/Title.tsx
@@ -0,0 +1,42 @@
+import { cva } from 'class-variance-authority'
+import { ReactNode, forwardRef } from 'react'
+import cn from 'classnames'
+
+const titleClasses = cva('font-bold', {
+	variants: {
+		level: {
+			1: 'text-[40px] leading-[1.2]',
+			2: 'text-[32px] leading-[1.35]',
+			3: 'text-2xl leading-[1.33]',
+			4: 'text-lg leading-[1.2]',
+			5: 'text-base leading-tight',
+			6: 'text-sm leading-[1.33]'
+		}
+	}
+})
+
+interface ITitleProps {
+	level?: 1 | 2 | 3 | 4 | 5 | 6
+	as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+	children: ReactNode
+	className?: string
+	[key: string]: any
+}
+
+const Title = forwardRef<HTMLHeadingElement, ITitleProps>(
+	({ as: Tag = 'h5', children, className, level = 5, ...rest }, ref) => {
+		return (
+			<Tag
+				{...rest}
+				className={cn(titleClasses({ level, className }))}
+				ref={ref}
+			>
+				{children}
+			</Tag>
+		)
+	}
+)
+
+Title.displayName = 'Title'
+
+export default Title
